6c89e870135274bcef5134cf72dfdaaa5cf4fe1f
Roger Gutsire <cc@Rogers-MacBook-Pro.local>
2022-10-06 18:16:18 -0700

    new commit

---
+++ chip.js
@@ -0,0 +1,7 @@
+let (firstName, lastName, age);
+firstName=('Dehante');
+lastName=('Chang');
+age=(21);
+console.log ("My Name is"+firstName+lastName);
+alert('I am'+age);
+console.log('Having Fun In Class')
